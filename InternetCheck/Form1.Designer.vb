319071a90b86f1a2a73dc3e4794a1096a83a332a
Kirschi <Kirschi94@gmx.net>
2022-02-24 03:35:49 +0100

    Rephrased an option

--- InternetCheck/Form1.Designer.vb
+++ InternetCheck/Form1.Designer.vb
@@ -272,9 +272,9 @@ Partial Class Form_Main
         Me.CheckBox_Autostart.AutoSize = True
         Me.CheckBox_Autostart.Location = New System.Drawing.Point(8, 133)
         Me.CheckBox_Autostart.Name = "CheckBox_Autostart"
-        Me.CheckBox_Autostart.Size = New System.Drawing.Size(409, 19)
+        Me.CheckBox_Autostart.Size = New System.Drawing.Size(333, 19)
         Me.CheckBox_Autostart.TabIndex = 7
-        Me.CheckBox_Autostart.Text = "Automatically start checking the internet connection on program launch"
+        Me.CheckBox_Autostart.Text = "Start checking the internet connection on program launch"
         Me.CheckBox_Autostart.UseVisualStyleBackColor = True
         '
         'CheckBox_LogSave
@@ -389,7 +389,7 @@ Partial Class Form_Main
         '
         Me.ContextMenuStrip_NotIcon.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowToolStripMenuItem, Me.StartCheckingToolStripMenuItem, Me.OptionsToolStripMenuItem, Me.ExitToolStripMenuItem})
         Me.ContextMenuStrip_NotIcon.Name = "ContextMenuStrip_NotIcon"
-        Me.ContextMenuStrip_NotIcon.Size = New System.Drawing.Size(181, 114)
+        Me.ContextMenuStrip_NotIcon.Size = New System.Drawing.Size(166, 92)
         '
         'ShowToolStripMenuItem
         '
@@ -413,43 +413,43 @@ Partial Class Form_Main
         'ConLostTSMI
         '
         Me.ConLostTSMI.Name = "ConLostTSMI"
-        Me.ConLostTSMI.Size = New System.Drawing.Size(378, 22)
+        Me.ConLostTSMI.Size = New System.Drawing.Size(321, 22)
         Me.ConLostTSMI.Text = "Connection lost sound"
         '
         'ConBackTSMI
         '
         Me.ConBackTSMI.Name = "ConBackTSMI"
-        Me.ConBackTSMI.Size = New System.Drawing.Size(378, 22)
+        Me.ConBackTSMI.Size = New System.Drawing.Size(321, 22)
         Me.ConBackTSMI.Text = "Connection reestablished sound"
         '
         'NotifyTSMI
         '
         Me.NotifyTSMI.Name = "NotifyTSMI"
-        Me.NotifyTSMI.Size = New System.Drawing.Size(378, 22)
+        Me.NotifyTSMI.Size = New System.Drawing.Size(321, 22)
         Me.NotifyTSMI.Text = "Show notification if connection status changes"
         '
         'LogSaveTSMI
         '
         Me.LogSaveTSMI.Name = "LogSaveTSMI"
-        Me.LogSaveTSMI.Size = New System.Drawing.Size(378, 22)
+        Me.LogSaveTSMI.Size = New System.Drawing.Size(321, 22)
         Me.LogSaveTSMI.Text = "Save log files"
         '
         'AutostartTSMI
         '
         Me.AutostartTSMI.Name = "AutostartTSMI"
-        Me.AutostartTSMI.Size = New System.Drawing.Size(378, 22)
-        Me.AutostartTSMI.Text = "Automatically start checking connection status on launch"
+        Me.AutostartTSMI.Size = New System.Drawing.Size(321, 22)
+        Me.AutostartTSMI.Text = "Start checking connection status on launch"
         '
         'StartMinTSMI
         '
         Me.StartMinTSMI.Name = "StartMinTSMI"
-        Me.StartMinTSMI.Size = New System.Drawing.Size(378, 22)
+        Me.StartMinTSMI.Size = New System.Drawing.Size(321, 22)
         Me.StartMinTSMI.Text = "Start minimized"
         '
         'WinStartTSMI
         '
         Me.WinStartTSMI.Name = "WinStartTSMI"
-        Me.WinStartTSMI.Size = New System.Drawing.Size(378, 22)
+        Me.WinStartTSMI.Size = New System.Drawing.Size(321, 22)
         Me.WinStartTSMI.Text = "Start on Windows startup"
         '
         'ExitToolStripMenuItem
